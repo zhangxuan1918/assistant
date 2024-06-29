759c573233c5ded986fcd14745bd3698862b57e5
xuan zhang <xuan@xuans-MacBook-Pro.local>
2024-06-29 21:13:26 +0200

    add audio manager, tts and stt services

--- audio/stt_service.py
+++ audio/stt_service.py
@@ -1,30 +1,69 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import os
+import threading
+from math import e
 import time
+from typing import Tuple
 from openai import OpenAI
-from audio.audio_manager import AudioManager, SpeechToTextResult, SpeechToTextTask
+from audio_manager import AudioManager, SpeechToTextResult, SpeechToTextTask
+
 
 @dataclass(frozen=True)
 class STTService:
     audio_manager: AudioManager
     url: str = "http://192.168.1.26:8000/v1"
     model_name: str = "Systran/faster-distil-whisper-large-v3"
+    stop_event: threading.Event = field(default_factory=threading.Event)
+    client: OpenAI = OpenAI(api_key="dummy key", base_url=url)
 
-    def __post_init__(self):
-        self.client: OpenAI = OpenAI(api_key="dummy key", base_url=self.url)
+    def run(self):
+        while not self.stop_event.is_set():
+            if self.audio_manager.has_pending_audio_to_text_tasks():
+                task = self.audio_manager.get_audio_to_text_task()
+                if task is not None and (text := self.convert(task)) is not None:
+                    self.audio_manager.save_audio_to_text_result(
+                        SpeechToTextResult(task, text)
+                    )
 
-    def main_loop(self):
-        while True:
-            if self.audio_manager.has_pending_tasks:
-                task = self.audio_manager.get_task()
-                text = self.send_request(task)
-                self.audio_manager.save_result(SpeechToTextResult(task, text))
-            else:
-                time.sleep(10)
+    def convert(self, task: SpeechToTextTask) -> str | None:
+        try:
+            audio_file = open(task.filepath, "rb")
+            transcript = self.client.audio.transcriptions.create(
+                model=self.model_name, file=audio_file
+            )
+            return transcript.text
+        except Exception as e:
+            print(f"Error converting audio to text: {e}")
+            return None
+        
+    def stop(self):
+        self.stop_event.set()
 
-    def convert(self, task: SpeechToTextTask) -> str:
-        audio_file = open(task.filepath, "rb")
-        transcript = self.client.audio.transcriptions.create(
-            model=self.model_name, file=audio_file
-        )
-        return transcript.text
+def start_stt(audio_manager: AudioManager) -> Tuple[STTService, threading.Thread]:
+    stt_service = STTService(audio_manager)
+    thread = threading.Thread(target=stt_service.run)
+    thread.start()
+    return stt_service, thread
 
+
+def stop_stt(stt_service: STTService, thread: threading.Thread) -> None:
+    stt_service.stop()
+    thread.join()
+
+
+if __name__ == "__main__":
+    filenames = ["audio.wav", "audio2.wav"]
+    folder = os.path.dirname(os.path.abspath(__file__))
+    
+    audio_manager = AudioManager(conversation_id="2")
+    stt_service, thread = start_stt(audio_manager=audio_manager)
+    for i, filename in enumerate(filenames):
+        print(f"processing {i}th audio")
+        task = SpeechToTextTask(task_id=str(i), filepath=os.path.join(folder, filename))
+        audio_manager.add_audio_to_text_task(task)
+        print(f"has pending speech to text task: {audio_manager.has_pending_audio_to_text_tasks()}")
+        while not audio_manager.has_audio_to_text_results(task=task):
+            time.sleep(1)
+        text = audio_manager.get_audio_to_text_result(task_id=task.task_id)
+        print(f"speech to text result: {text}")
+    stop_stt(stt_service=stt_service, thread=thread)
---
+++ audio/tts_service.py
@@ -0,0 +1,89 @@
+from ast import Tuple
+from dataclasses import dataclass, field
+from re import T
+import threading
+import time
+from typing import Any, Dict, List, Tuple
+from openai import audio
+import requests
+from audio_manager import AudioManager, TextToSpeechTask, TextToSpeechResult
+from util import play_audio
+
+
+@dataclass(frozen=True)
+class TTSService:
+    audio_manager: AudioManager
+    url: str = "http://192.168.1.26:9966/tts"
+    voice: str = "3333"
+    temperature: float = 0.3
+    top_p: float = 0.7
+    top_k: int = 20
+    skip_refine: int = 0
+    custom_voice: int = 0
+    stop_event: threading.Event = field(default_factory=threading.Event)
+
+    def run(self):
+        while not self.stop_event.is_set():
+            if self.audio_manager.has_pending_text_to_audio_tasks():
+                task = self.audio_manager.get_text_to_audio_task()
+                if task is not None:
+                    raw_response = self.convert(task)
+                    self.audio_manager.save_text_to_audio_result(
+                        TextToSpeechResult(task, raw_response)
+                    )
+
+    def convert(self, task: TextToSpeechTask) -> Dict[Any, Any]:
+        try:
+            return requests.post(
+                self.url,
+                data={
+                    "text": task.text,
+                    "prompt": "",
+                    "voice": self.voice,
+                    "temperature": self.temperature,
+                    "top_p": self.top_p,
+                    "top_k": self.top_k,
+                    "skip_refine": self.skip_refine,
+                    "custom_voice": self.custom_voice,
+                },
+            ).json()
+        except Exception as e:
+            print(f"Error converting text to audio: {e}")
+            return {"code": 1, "msg": "error", "error": e}
+
+    def stop(self):
+        self.stop_event.set()
+
+
+def start_tts(audio_manager: AudioManager) -> Tuple[TTSService, threading.Thread]:
+    tts_service = TTSService(audio_manager)
+    thread = threading.Thread(target=tts_service.run)
+    thread.start()
+    return tts_service, thread
+
+
+def stop_tts(tts_service: TTSService, thread: threading.Thread) -> None:
+    tts_service.stop()
+    thread.join()
+
+
+if __name__ == "__main__":
+    # texts = ["Hi, my name is George! very nice to meet you", "did you eat? If not, how about some noodles?"]
+    # texts = ["did you eat? If not, how about some noodles?", "Hi, my name is George! very nice to meet you"]
+    # texts = ["Hi, my name is George! very nict to meet you"]
+    # texts = ["did you eat? If not, how about some noodles?"]
+    texts = ["Recording audio from a microphone using Python is tricky! Why? Because Python doesn't provide a standard library for it. Existing third-party libraries (e.g. PyAudio) are not cross-platform and have external dependencies."]
+
+    audio_manager = AudioManager(conversation_id="1")
+    tts_service, thread = start_tts(audio_manager=audio_manager)
+    for i, text in enumerate(texts):
+        print(f"processing {i}th text: {text}")
+        task = TextToSpeechTask(task_id=str(i), text=text)
+        audio_manager.add_text_to_audio_task(task)
+        while not audio_manager.has_text_to_audio_results(task=task):
+            time.sleep(1)
+        file_urls: List[str] = audio_manager.get_text_to_audio_result(task.task_id)
+        for url in file_urls:
+            print(url)
+            play_audio(url=url)
+    stop_tts(tts_service=tts_service, thread=thread)
